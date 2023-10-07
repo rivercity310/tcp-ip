ee47b6815dd9357e24085183d47a53df6ca73864
rivercity310 <h970126@gmail.com>
2023-10-07 19:58:54 +0900

    TCP Client Socket / Server Socket 생성 & 접속 & Server Socket에 Telnet 접속

--- window_socket/main.cpp
+++ window_socket/main.cpp
@@ -2,11 +2,13 @@ extern void InitWinSock();
 extern void ByteOrder();
 extern void IpAddr();
 extern void DNSTest();
+extern void TCPClient();
 
 int main(int argc, char* argv[])
 {
 	// InitWinSock();
 	// ByteOrder();
-	IpAddr();
+	// IpAddr();
 	// DNSTest();
+	TCPClient();
 }
