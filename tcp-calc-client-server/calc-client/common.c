a77ae77a5cefd6f1db6e474da8f00e5a0d83344e
rivercity310 <h970126@gmail.com>
2023-10-13 02:15:29 +0900

    TCP CLIENT & SERVER 계산기 과제

---
+++ tcp-calc-client-server/calc-client/common.c
@@ -0,0 +1,57 @@
+#include "common.h"
+
+void err_quit(const char* msg) {
+	LPVOID lpMsgBuf;
+
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL,
+		WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf,
+		0,
+		NULL
+	);
+
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+// 소켓 함수 오류 출력
+void err_display(const char* msg) {
+	LPVOID lpMsgBuf;
+
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL,
+		WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf,
+		0,
+		NULL
+	);
+
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+/*
+void err_display(int errorcode) {
+	LPVOID lpMsgBuf;
+
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL,
+		errorcode,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf,
+		0,
+		NULL
+	);
+
+	printf("[오류] %s\n", (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+*/
---
+++ tcp-calc-client-server/calc-server/main.c
@@ -0,0 +1,5 @@
+extern void TcpServerStart();
+
+int main() {
+	TcpServerStart();
+}
