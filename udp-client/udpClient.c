f83e657575699bd4d6674087e753911e57685d57
rivercity310 <h970126@gmail.com>
2023-11-21 09:59:26 +0900

    IPv4, v6 동시지원 멀티스레드 UDP 서버

--- udp-client/udpClient.c
+++ udp-client/udpClient.c
@@ -3,11 +3,16 @@
 #include <ws2tcpip.h>
 #include <winsock2.h>
 
-#define SERVERADDR "127.0.0.1"
-#define SERVERPORT 9000
+#define SERVERADDR4  "127.0.0.1"
+#define SERVERPORT4  9000
+#define SERVERADDR6  "::1"
+#define SERVERPORT6  9090
 #define BUFSIZE    512
 
 #pragma comment(lib, "ws2_32")
+
+static void UDPClient4();
+static void UDPClient6();
 
 void UDPClient() {
 	WSADATA wsa;
@@ -15,6 +20,18 @@ void UDPClient() {
 		exit(1);
 	}
 
+	int version;
+	printf("연결할 IP Version을 선택하세요: ");
+	scanf_s("%d", &version);
+	getchar();
+
+	if (version == 4) UDPClient4();
+	else UDPClient6();
+
+	WSACleanup();
+}
+
+static void UDPClient4() {
 	// 소켓 생성
 	SOCKET sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (sock == INVALID_SOCKET) exit(1);
@@ -24,8 +41,8 @@ void UDPClient() {
 	// 소켓 주소 구조체 초기화
 	struct sockaddr_in serveraddr;
 	memset(&serveraddr, 0, sizeof(serveraddr));
-	inet_pton(AF_INET, SERVERADDR, &serveraddr.sin_addr);
-	serveraddr.sin_port = htons(SERVERPORT);
+	inet_pton(AF_INET, SERVERADDR4, &serveraddr.sin_addr);
+	serveraddr.sin_port = htons(SERVERPORT4);
 	serveraddr.sin_family = PF_INET;
 
 	// 데이터 통신에 사용할 변수
@@ -77,5 +94,68 @@ void UDPClient() {
 	}
 
 	closesocket(sock);
-	WSACleanup();
+}
+
+static void UDPClient6() {
+	SOCKET sock = socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == INVALID_SOCKET) exit(1);
+
+	int retval;
+
+	// 서버 구조체 초기화
+	struct sockaddr_in6 serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	inet_pton(AF_INET6, SERVERADDR6, &serveraddr.sin6_addr);
+	serveraddr.sin6_family = AF_INET6;
+	serveraddr.sin6_port = htons(SERVERPORT6);
+
+	// 데이터 통신에 사용할 변수
+	struct sockaddr_in6 peeraddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];
+	int len;
+
+	// 서버와 데이터 통신
+	while (1) {
+		// 데이터 입력
+		printf("\n[보낼 데이터] ");
+		if (fgets(buf, BUFSIZE + 1, stdin) == NULL) {
+			break;
+		}
+
+		// 스페이스바 제거
+		len = (int)strlen(buf);
+		if (buf[len - 1] == '\n') {
+			buf[len - 1] = '\0';
+		}
+		if (strlen(buf) == 0) {
+			break;
+		}
+
+		// 데이터 전송
+		retval = sendto(sock, buf, (int)strlen(buf), 0,
+			(struct sockaddr*)&serveraddr, sizeof(serveraddr));
+		if (retval == SOCKET_ERROR) exit(1);
+
+		printf("[UDP Client] %d바이트 전송 완료\n", retval);
+
+		// 데이터 받기
+		addrlen = sizeof(peeraddr);
+		retval = recvfrom(sock, buf, BUFSIZE, 0,
+			(struct sockaddr*)&peeraddr, &addrlen);
+		if (retval == SOCKET_ERROR) exit(1);
+
+		// 송신지 IP 주소 체크
+		if (memcmp(&peeraddr, &serveraddr, sizeof(peeraddr))) {
+			printf("[오류] 잘못된 데이터입니다!\n");
+			break;
+		}
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		printf("[UDP Client] %d바이트를 받았습니다.\n", retval);
+		printf("[받은 데이터] %s\n", buf);
+	}
+
+	closesocket(sock);
 }
--- udp-server/udpServer.c
+++ udp-server/udpServer.c
@@ -5,10 +5,14 @@
 #include <tchar.h>
 #include <string.h>
 
-#define SERVERPORT 9000
-#define BUFSIZE    512
+#define SERVERPORT   9000
+#define SERVERPORT6  9090
+#define BUFSIZE      512
 
 #pragma comment(lib, "ws2_32")
+
+static DWORD WINAPI UDPServer4(LPVOID args);
+static DWORD WINAPI UDPServer6(LPVOID args);
 
 void UDPServer() {
 	WSADATA wsa;
@@ -16,6 +20,14 @@ void UDPServer() {
 		exit(1);
 	}
 
+	HANDLE handle[2];
+	handle[0] = (HANDLE)_beginthreadex(NULL, 0, UDPServer4, NULL, 0, NULL);
+	handle[1] = (HANDLE)_beginthreadex(NULL, 0, UDPServer6, NULL, 0, NULL);
+
+	WaitForMultipleObjects(2, handle, TRUE, INFINITE);
+}
+
+static DWORD WINAPI UDPServer4(LPVOID args) {
 	int retval;
 
 	// 소켓 생성
@@ -57,6 +69,47 @@ void UDPServer() {
 	}
 
 	closesocket(sock);
-	WSACleanup();
+	return;
+}
+
+static DWORD WINAPI UDPServer6(LPVOID args) {
+	int retval;
+	
+	SOCKET sock = socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == INVALID_SOCKET) exit(1);
+
+	// bind()
+	struct sockaddr_in6 serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin6_family = AF_INET6;
+	serveraddr.sin6_addr = in6addr_any;
+	serveraddr.sin6_port = htons(SERVERPORT6);
+	retval = bind(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) exit(1);
+
+	// 데이터 통신에 사용할 변수
+	struct sockaddr_in6 clientaddr;
+	int addrlen;
+	char buf[BUFSIZE + 1];
+
+	while (1) {
+		addrlen = sizeof(clientaddr);
+		retval = recvfrom(sock, buf, BUFSIZE, 0,
+			(struct sockaddr*)&clientaddr, &addrlen);
+		if (retval == SOCKET_ERROR) exit(1);
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		char addr[INET6_ADDRSTRLEN];
+		inet_ntop(AF_INET6, &clientaddr.sin6_addr, addr, sizeof(addr));
+		printf("[UDP/%s:%d] %s\n", addr, ntohs(clientaddr.sin6_port), buf);
+
+		// 데이터 보내기
+		retval = sendto(sock, buf, retval, 0,
+			(struct sockaddr*)&clientaddr, sizeof(clientaddr));
+		if (retval == SOCKET_ERROR) exit(1);
+	}
+
+	closesocket(sock);
 	return;
 }
